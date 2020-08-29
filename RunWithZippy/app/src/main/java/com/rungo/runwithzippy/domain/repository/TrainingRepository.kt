97bc62ad6202652df22ab21bb5c9e83a58cbc987
Alexey Bakycharov <abakycharov@aeb-it.ru>
2020-08-29 14:26:51 +0900

    Modify TrainingRepository.kt

--- RunWithZippy/app/src/main/java/com/rungo/runwithzippy/domain/repository/TrainingRepository.kt
+++ RunWithZippy/app/src/main/java/com/rungo/runwithzippy/domain/repository/TrainingRepository.kt
@@ -1,2 +1,7 @@
 package com.rungo.runwithzippy.domain.repository
 
+import com.rungo.runwithzippy.data.model.AccessToken
+
+interface TrainingRepository {
+    suspend fun getTrainings(accessToken: AccessToken)
+}
