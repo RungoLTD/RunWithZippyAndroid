6ddadd7611f9d02920edf8d04f6ba62bf5e5f936
Bugenmode <bugenmode@gmail.com>
2020-10-31 19:58:08 +0900

    Add achievement layout, change navigation drawer

---
+++ RunWithZippy/app/src/main/java/com/rungo/runwithzippy/presentation/features/achievements/AchievementActivity.kt
@@ -0,0 +1,12 @@
+package com.rungo.runwithzippy.presentation.features.achievements
+
+import com.rungo.runwithzippy.base.BaseActivity
+
+class AchievementActivity : BaseActivity() {
+
+    override fun exit() {
+
+    }
+
+
+}
---
+++ RunWithZippy/app/src/main/java/com/rungo/runwithzippy/presentation/features/achievements/AchievementViewModel.kt
@@ -0,0 +1,7 @@
+package com.rungo.runwithzippy.presentation.features.achievements
+
+import com.rungo.runwithzippy.base.BaseViewModel
+
+class AchievementViewModel : BaseViewModel() {
+
+}
